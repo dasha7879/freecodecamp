37511bb4d7d50a54e52c6fe10b6a483289a6d3a4
dasha7879 <belka7879@gmail.com>
2022-12-11 00:32:09 +0300

    feat: solve task 8

---
+++ 08.js
@@ -0,0 +1,10 @@
+//Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver= 9000;
