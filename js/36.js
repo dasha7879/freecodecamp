93c3925d804748f1aaecf3054a6697b5cde1a73a
dasha7879 <belka7879@gmail.com>
2022-12-11 13:48:34 +0300

    feat: solve task 36

---
+++ js/36.js
@@ -0,0 +1,6 @@
+//Use Bracket Notation to Find the Last Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
