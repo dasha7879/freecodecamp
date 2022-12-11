e8c630e626dc766329944dda74e2e314665139fe
dasha7879 <belka7879@gmail.com>
2022-12-11 13:37:29 +0300

    feat: solve task 33

---
+++ js/33.js
@@ -0,0 +1,7 @@
+//Use Bracket Notation to Find the First Character in a String
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
