57f090b12936d01317f2f983a348ca590af7c855
dasha7879 <belka7879@gmail.com>
2022-12-13 15:25:14 +0300

    feat: solve task 62-70

---
+++ js/64.js
@@ -0,0 +1,10 @@
+//Comparison with the Strict Inequality Operator
+// Setup
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
