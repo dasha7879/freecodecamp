2008c1a6d1b4afca64bf5f1d28db39269276173e
dasha7879 <belka7879@gmail.com>
2022-12-16 16:05:00 +0300

    feat: add 90-91 without solves

--- js/91.js
+++ js/91.js
@@ -1,1 +1,15 @@
-// Setup
+// // Setup
+// function checkObj(obj, checkProp) {
+//     // Only change code below this line
+//     obj = {
+//      prop: thisProp
+//      }
+//      if(obj.hasOwnProperty(checkProp)){
+//        return obj[prop]
+//      }
+//         return "Not Found"
+     
+   
+//     // Only change code above this line
+//   }
+//   console.log(({pet: "kitten", bed: "sleigh"}, "gift"))
